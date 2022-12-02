60452e014ea11b52dceaaf10f657a764f609887d
horvat.adam <horvat.adam@28-15>
2022-12-02 15:08:05 +0100

    Add project files.

---
+++ Beszelgetes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cseveges
+{
+    internal class Beszelgetes
+    {
+        public DateTime Kezdet;
+        public DateTime Veg;
+        public string Kezdemenyezo;
+        public string Fogado;
+    public Beszelgetes(string sor)
+    {
+            var s = sor.Split(';');
+            this.Kezdet = DateTime.ParseExact(s[0], "yy.MM.dd-HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            this.Veg = DateTime.ParseExact(s[1], "yy.MM.dd-HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            this.Kezdemenyezo = s[2];
+            this.Fogado = s[3];
+
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Cseveges
+{
+    internal class Program
+    {
+        static List<Beszelgetes> Beszel_List = new();
+        static List<string> TagokLs = new();
+        static void Main(string[] args)
+        {
+            Feladat03();
+            Feladat04();
+
+        }
+
+        private static void Feladat04()
+        {
+            
+        }
+
+        private static void Feladat03()
+        {
+            StreamReader sr = new(@"..\..\..\src\csevegesek.txt");
+            StreamReader sr2 = new(@"..\..\..\src\tagok.txt"); 
+            int sz = 0;
+            while (!sr.EndOfStream)
+            {
+                try
+                {Beszel_List.Add(new Beszelgetes(sr.ReadLine()));}
+                catch (Exception)
+                {}
+                
+            }
+            while (!sr2.EndOfStream)
+            {
+                TagokLs.Add(sr2.ReadLine());
+            }
+        }
+    }
+}
